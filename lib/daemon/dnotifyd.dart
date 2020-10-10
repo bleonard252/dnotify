9323436c5b82bd7354de9cbf075329532ce8a88a
Blake <bleonard252@rocketmail.com>
2020-10-10 15:43:28 -0400

    Asynchronize dnotifyd

--- lib/daemon/dnotifyd.dart
+++ lib/daemon/dnotifyd.dart
@@ -3,18 +3,17 @@ import 'dart:io';
 
 ServerSocket dnotifySock;
 
-void start() {
-  //File.fromUri(Uri.file("/tmp/dnotify.sock")).createSync();
-  ServerSocket.bind(InternetAddress("/tmp/dnotify.sock", type: InternetAddressType.unix), 0)
-  .then((value) {
-    dnotifySock = value;
-    dnotifySock.listen((event) {
-      utf8.decoder.bind(event).listen((strdata) {
-        //ignore:unused_local_variable
-        var data = jsonDecode(strdata);
-        print(strdata);
-      });
+void start() async {
+  File.fromUri(Uri.file("/tmp/dnotify-live.json")).openWrite();
+  dnotifySock = await ServerSocket.bind(InternetAddress("/tmp/dnotify.sock", type: InternetAddressType.unix), 0);
+  dnotifySock.listen((event) {
+    utf8.decoder.bind(event).listen((strdata) {
+      //ignore:unused_local_variable
+      var data = jsonDecode(strdata);
+      print(strdata);
     });
-  })
-  .catchError((error) => throw error);
+  });
 }
+
+//TODO: implement a D-Bus interface for org.freedesktop.Notifications
+// (or listen to and convert libnotify messages)
