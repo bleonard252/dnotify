7aaa0b1250833979c172b58460a1a345e1864e0d
Blake <bleonard252@rocketmail.com>
2020-10-11 14:26:09 -0400

    Add cancel method & docs to module

--- lib/dnotify.dart
+++ lib/dnotify.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dnotify/daemon/dnotifyd.dart' as _daemon;
 import 'package:dnotify/tool/src/send.dart' as _send;
+import 'package:dnotify/tool/src/send_cancel.dart' as _cancel;
 
 /// Register a notification listener.
 abstract class DNotifyDaemon {
@@ -15,8 +16,9 @@ abstract class DNotifyDaemon {
   }
 }
 
-/// Send notifications.
+/// Send and manage notifications.
 abstract class DNotify {
+  /// Send a notification.
   static Future<void> send(
     String title, 
     String text,
@@ -31,5 +33,11 @@ abstract class DNotify {
       priority: priority, 
       useTcp: useTcp != null ? useTcp : Platform.isLinux ? false : true
     );
+  }
+  /// Cancel a notification by its ID.
+  /// Will succeed as long as dnotifyd is running
+  /// and the cancel request can be sent.
+  static Future<void> cancel(String id) async {
+    await _cancel.send_cancel(id);
   }
 }
